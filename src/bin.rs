76c32df702b1300833ef0ebcba2c7908eb6a2de0
kyserd <kyrill@kyserd.com>
2026-08-12 21:23:27 -0400

    actually make a cli for easier testing

--- src/bin.rs
+++ src/bin.rs
@@ -1,41 +1,50 @@
-use ratatui::{DefaultTerminal, Frame};
-use std::io;
+use iroh_lan::{RouterIp, network::Network};
+use tokio::time::sleep;
 
-fn main() -> std::io::Result<()> {
-    ratatui::run(|terminal| App::default().run(terminal))?;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    if !self_runas::is_elevated() {
+        self_runas::admin()?;
+        return Ok(());
+    }
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_thread_ids(true)
+        .init();
+
+    let args: Vec<String> = std::env::args().collect();
+    let args_ref: Vec<&str> = args.iter().skip(1).map(String::as_str).collect();
+    match args_ref.as_slice() {
+        ["s", name, password] => {
+            let network = Network::new(name, password).await?;
+
+            while matches!(
+                network.get_router_state().await?,
+                RouterIp::NoIp | RouterIp::AquiringIp(_, _)
+            ) {
+                sleep(std::time::Duration::from_millis(500)).await;
+            }
+
+            println!("my ip is {:?}", network.get_router_state().await?);
+
+            tokio::spawn(async move {
+                loop {
+                    println!(
+                        "Network started with endpoint ID {:?}",
+                        network.get_router_state().await
+                    );
+                    sleep(std::time::Duration::from_secs(5)).await;
+                }
+            });
+
+            let _ = tokio::signal::ctrl_c().await;
+        }
+        ["j", name, password] => {
+            todo!()
+        }
+        _ => eprintln!("unknown args: {args_ref:#?}"),
+    }
+
     Ok(())
-}
-
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
-}
-
-impl App {
-    /// runs the application's main loop until the user quits
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget("hello world", frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        let event = crossterm::event::read()?;
-        let crossterm::event::Event::Key(key_event) = event else {
-            return Ok(());
-        };
-        match key_event.code {
-            crossterm::event::KeyCode::Esc => self.exit = true,
-
-            _ => {}
-        }
-        Ok(())
-    }
 }
